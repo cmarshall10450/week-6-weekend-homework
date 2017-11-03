c24271f65022206281ef13d4a153d2c21a026df9
Chris Marshall <cmarshall10450@gmail.com>
2017-11-03 16:10:07 +0000

    Test guitarStrapHasLengthOf100 and guitarStrapIsOfItemTypeAccessory passing.

--- tests/misc/GuitarStrapTest.java
+++ tests/misc/GuitarStrapTest.java
@@ -1,10 +1,31 @@
 package misc;
 
+import org.junit.Before;
+import org.junit.Test;
+import shop.ItemType;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by chris on 03/11/2017.
  */
 public class GuitarStrapTest {
 
+  GuitarStrap guitarStrap;
 
+  @Before
+  public void before() {
+    guitarStrap = new GuitarStrap(100, 20, 30, "Guitar Strap");
+  }
+
+  @Test
+  public void guitarStrapHasLengthOf100() {
+    assertEquals(100, guitarStrap.getLength());
+  }
+
+  @Test
+  public void guitarStrapIsOfItemTypeAccessory() {
+    assertEquals(ItemType.ACCESSORY, guitarStrap.getItemType());
+  }
 
 }
