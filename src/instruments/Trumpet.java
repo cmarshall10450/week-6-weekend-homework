7bfaa884f799c6db97cf49738057e6d7c3def904
Chris Marshall <cmarshall10450@gmail.com>
2017-11-03 11:14:17 +0000

    Test trumpetHasFiveValves passing

---
+++ src/instruments/Trumpet.java
@@ -0,0 +1,17 @@
+package instruments;
+
+/**
+ * Created by chris on 03/11/2017.
+ */
+public class Trumpet {
+
+  int numValves;
+
+  public Trumpet(int numValves) {
+    this.numValves = numValves;
+  }
+
+  public int getNumValves() {
+    return numValves;
+  }
+}
---
+++ src/tests/TrumpetTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import instruments.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chris on 03/11/2017.
+ */
+public class TrumpetTest {
+
+  Trumpet trumpet;
+
+  @Before
+  public void before() {
+    trumpet = new Trumpet(5);
+  }
+
+  @Test
+  public void trumpetHasFiveValves() {
+    assertEquals(5, trumpet.getNumValves());
+  }
+
+}
